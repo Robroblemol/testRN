55401bdc54e126cb354344af45b288f400ba989b
Raziel <robroblemol>
2018-06-30 14:30:08 -0500

    nada

--- src/components/Contacts/ContactCard/index.js
+++ src/components/Contacts/ContactCard/index.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
-const ContactCard = ({ name, lastName, age, phone }) => (
+
+const ContactCard= ({ name, lastName, age, phone }) => (
   <View>
-    <Text>{name}</Text>
-    <Text>{lastName}</Text>
-    <Text>{age}</Text>
-    <Text>{phone}</Text>
+      {
+          <View>
+            <Text>{name}</Text>
+            <Text>{lastName}</Text>
+            <Text>{age}</Text>
+            <Text>{phone}</Text>
+          </View>
+      }
   </View>
 );
 
-ContactCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-  age: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
-};
+ ContactCard.propTypes = {
+   name: PropTypes.String.isRequired,
+   lastName:PropTypes.String.isRequired,
+   age:PropTypes.String.isRequired,
+   phone:PropTypes.String.isRequired,
+ };
 
 export default ContactCard;
